e03fe4f1af835e52527f98373108831e5bd3a115
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-17 04:54:37 +0000

    white space fix

--- main.cs
+++ main.cs
@@ -24,13 +24,13 @@ class Program
         randomNumber = rnd.Next(1, 6);
 
         Console.WriteLine("");
-        if (userNumber == randomNumber) {
+        if (userNumber == randomNumber){
         Console.WriteLine("The answer was: " + randomNumber + ". You got it right!");
-        }
+      }
 
-      if (userNumber != randomNumber) {
+        if (userNumber != randomNumber){
         Console.WriteLine("The answer was: " + randomNumber + ". You got it wrong. Better luck next time!");
-        }
+      }
 
         Console.WriteLine("\nDone.");
     }
